1afc6739eb0da912660d9fc19a1660d2394fbcbd
mateusb12 <matbessa12@gmail.com>
2026-06-02 21:51:50 -0300

    refactor(overall): Fix GitHub Actions environment variable naming for deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,11 @@
 import type { NextConfig } from "next"
 
 const repositoryName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "zenit"
-const isGithubPagesBuild = process.env.GITHUB_ACTIONS === "true"
+const isGithubActions = process.env.GITHUB_ACTIONS === "true"
 const isUserOrOrgPagesSite = repositoryName.endsWith(".github.io")
+
 const githubPagesBasePath =
-  isGithubPagesBuild && !isUserOrOrgPagesSite ? `/${repositoryName}` : ""
+  isGithubActions && !isUserOrOrgPagesSite ? `/${repositoryName}` : ""
 
 const nextConfig: NextConfig = {
   output: "export",
